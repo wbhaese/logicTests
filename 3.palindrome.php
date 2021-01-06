e8c6c8ece6c441c2761d67eab0e6877ea20acdb6
Wallace Haese <wallace@gamingbuddy.gg>
2021-01-06 19:03:24 -0300

    add a hard way to figure out a palindrome

--- 3.palindrome.php
+++ 3.palindrome.php
@@ -1,6 +1,20 @@
-
+<?php
 function checkPalindrome($inputString) {
     
-    return ($inputString == strrev($inputString));    
+    // Easy way:
+    
+    return ($inputString == strrev($inputString));  //strrev() function revert string 
+    
+    //Hard way:)
+    
+    $stringArr = str_split($inputString); 
+
+    $revArray = [];
+    for($i = count($stringArr) -1; $i >= 0; $i-- ){
+        array_push($revArray, $stringArr[$i]);        
+    }
+    
+    // var_dump(implode($revArray));
+    return ($inputString == implode($revArray));
 
 }
